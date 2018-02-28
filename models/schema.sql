6546b78248b85efdf956d9bc18fe7c397c2bbe31
Chris Clark <christopheryc94@gmail.com>
2018-02-27 19:51:48 -0700

    changed schema outline

--- models/schema.sql
+++ models/schema.sql
@@ -5,12 +5,14 @@ USE parkingspots_db;
 
 CREATE TABLE parkingspots (
   id INT NOT NULL,
-  price INT NOT NULL,
-  location VARCHAR(150) NOT NULL,
-  user_name VARCHAR(100) NOT NULL,
-  booked_name VARCHAR(100) NULL,
+  price DECIMAL NOT NULL,
+  location_lat DECIMAL NOT NULL,
+  location_lng DECIMAL NOT NULL,
+  address VARCHAR(150) NOT NULL,
+  owner_username VARCHAR(100) NOT NULL,
+  bookedBy_username VARCHAR(100) NULL,
   time_booked DATETIME NULL,
-  time_left DATETIME NULL,
+  time_vacated DATETIME NULL,
   available BOOLEAN DEFAULT TRUE,
   PRIMARY KEY (id)
 );
