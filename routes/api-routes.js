ccb329b86474d9d1a03ec6733a02364c20e7b2dc
ddmartin3 <darwood.martin@yahoo.com>
2018-03-03 12:38:01 -0700

    commit for pull

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,6 +6,26 @@ module.exports = function(app) {
   app.post('/api/space', function(req, res) {
     db.Space.create(req.body).then(function(dbSpace) {
       res.json(dbSpace);
+      console.log(dbSpace.dataValues);
     });
   });
+
+
+  //PUT route for updating parking spot
+  // app.put("/api/space/:id", function(req, res) {
+  //     db.Space.update(req.body,
+  //     {
+  //         where: {
+  //             id: req.body.id
+  //         }
+  //     }).then(function(dbSpace) {
+  //         Space.updateAttributes({
+  //             availability: false,
+  //             bookedBy_username: req.body.bookedBy_username,
+  //             time_booked: SEQUELIZE.DATE
+  //         })
+  //         res.json(dbPost);
+  //     })
+  // })
+
 };
