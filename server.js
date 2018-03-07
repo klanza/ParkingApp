0d31b59d13a1a4a8666524855f8d8205dfbbabbc
klanza <klanza91@gmail.com>
2018-03-07 01:26:30 -0700

    Removed force:true

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require('./routes/html-routes.js')(app);
 // app.use(routes);
 
 // Sync sequelize models and start app
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
   });
