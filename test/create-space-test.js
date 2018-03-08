ef78b53f0a3ac6e15cfb37b98b43f1a10e955947
klanza <klanza91@gmail.com>
2018-03-08 14:48:58 -0700

    Further work on function tests

--- test/create-space-test.js
+++ test/create-space-test.js
@@ -2,7 +2,7 @@ const Nightmare = require('nightmare');
 const expect = require('chai').expect;
 
 describe('Park Place', function() {
-    this.timeout(20000)
+    this.timeout(20000);
     it('should create a new parking space, and log it to the database', function(done) {
         Nightmare({show: true})
             .goto('https://limitless-peak-93129.herokuapp.com/new')
@@ -11,18 +11,17 @@ describe('Park Place', function() {
             // Enter address for space.
             .type('#newSpaceAddress', '732 E. University Blvd.')
             // Enter coordinates for space.
-            .type('#newCoordinate', '32.23123662280256 -110.95997386642557')
+            .click('#map')
+            // .type('#newCoordinate', '32.23123662280256 -110.95997386642557')
             // Enter price for space.
             .type('#newSpacePrice', '5.00')
             // Click the submit button to enter location into datbase
             .click('#submit')
-            .evaluate(function() {
-                return dbSpace.dataValues
-            })
-            .then(function(space) {
-                expect(space.availability).to.equal(true);
-                expect(space.availability.location_lat).to.equal('32.22992152310982');
-                expect(space.availability.location_lng).to.equal('-110.96134382019045');
+            .then(function(newSpace) {
+                expect(newSpace.availability).to.equal(true);
+                expect(newSpace.owner_username).to.equal('nightmaretest@fakesite.com');
+                expect(newSpace.address).to.equal('732 E. University Blvd.');
+                expect(newSpace.price).to.equal('5.00');
                 done();
             });
     });
