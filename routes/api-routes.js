671a19b8c82ea4312c9c94771813e2b0da2a075c
Kenneth Lanza <klanza91@gmail.com>
2018-03-20 19:44:04 -0700

    Corrected routing for checkout to new Heroku URL

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -71,7 +71,7 @@ module.exports = function(app) {
               to: dbSpace.bookedBy_username,
               subject: 'You have booked a space at ' + dbSpace.address + '!',
               html: '<h1>Thanks for Using Park Place</h1><p>When you are done with your space, please check out at the link below. </p>' +
-                '<br/> <a href="https://limitless-peak-93129.herokuapp.com/checkout/' + dbSpace.id + '">Check Out at Park Place</a>',
+                '<br/> <a href="https://parkez.herokuapp.com/checkout/' + dbSpace.id + '">Check Out at Park Place</a>',
               // text: 'The price per hour of the space you have rented is $' + dbSpace.price + '/hr.',
             };
 
